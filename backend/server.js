458c346936ed1e32abd3eae7eb044df4ee371b1a
Venkatesh goud <goudvenki5@gmail.com>
2026-03-30 16:12:45 +0530

    open ai response commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import routes from './routes/routes.js';
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/",routes);
 
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
@@ -24,6 +27,12 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/evaluate',async(req,res)=>{
+    const {startUpInput} = req.body;
+    
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
